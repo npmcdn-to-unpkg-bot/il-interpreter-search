641c39241b18046cfd7da55895206d51f17f7105
Rene M. Paccha <paccha@gmail.com>
2016-03-20 20:34:20 -0500

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: "./app/assets/frontend/main.jsx",
+    resolve: {
+        extensions: ["", ".js", ".jsx"]
+    },
+    output: {
+        path: __dirname + "/app/assets/javascripts/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: ["react", "es2015"]
+                }
+            }
+        ]
+    }        
+};
